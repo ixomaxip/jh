eec275d1a1b0063e13e85fd1e7e446f138b75051
Max <max.white.rabbit@gmail.com>
2020-04-04 20:06:26 +0500

    try to run: ok? but need to pull docker image

--- ci/config/jupyterhub_config.py
+++ ci/config/jupyterhub_config.py
@@ -4,6 +4,7 @@ import os
 
 c.JupyterHub.authenticator_class = 'nativeauthenticator.NativeAuthenticator'
 c.Authenticator.admin_users = {'admin'}
+# c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
 
 # c.JupyterHub.hub_ip = os.environ['HUB_IP']
 # c.JupyterHub.services = [
@@ -14,5 +15,23 @@ c.Authenticator.admin_users = {'admin'}
 #     },
 # ]
 
-# c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+
+# we need the hub to listen on all ips when it is in a container
+c.JupyterHub.hub_ip = '0.0.0.0'
+# the hostname/ip that should be used to connect to the hub
+# this is usually the hub container's name
+# c.JupyterHub.hub_connect_ip = 'jupyterhub'
+
+# pick a docker image. This should have the same version of jupyterhub
+# in it as our Hub.
+c.DockerSpawner.image = 'jupyter/base-notebook'
+
+# tell the user containers to connect to our docker network
+c.DockerSpawner.network_name = 'rt_default'
+
+# delete containers when the stop
+c.DockerSpawner.remove = True
+
+# c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
 # c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
